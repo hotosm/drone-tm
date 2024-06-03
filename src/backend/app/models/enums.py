d484283d55fa5e96a0f905faff422a349b1d94e2
Niraj Adhikari <nrjadkry@gmail.com>
2024-06-03 10:31:25 +0545

    added http status class in enums

--- src/backend/app/models/enums.py
+++ src/backend/app/models/enums.py
@@ -82,3 +82,25 @@ class ProjectPriority(IntEnum, Enum):
     HIGH = 1
     MEDIUM = 2
     LOW = 3
+
+
+class HTTPStatus(IntEnum):
+    """All HTTP status codes used in endpoints."""
+
+    # Success
+    OK = 200
+    CREATED = 201
+    ACCEPTED = 202
+    NO_CONTENT = 204
+
+    # Client Error
+    BAD_REQUEST = 400
+    UNAUTHORIZED = 401
+    FORBIDDEN = 403
+    NOT_FOUND = 404
+    CONFLICT = 409
+    UNPROCESSABLE_ENTITY = 422
+
+    # Server Error
+    INTERNAL_SERVER_ERROR = 500
+    NOT_IMPLEMENTED = 501
