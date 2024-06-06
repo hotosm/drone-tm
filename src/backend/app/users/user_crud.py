5fd565f094e1a85f0c8ffef29d0ea9a79f48d3a5
Niraj Adhikari <nrjadkry@gmail.com>
2024-06-06 22:58:02 +0545

    feat: login api

---
+++ src/backend/app/users/user_crud.py
@@ -0,0 +1,43 @@
+import jwt
+from app.config import settings
+from datetime import datetime, timedelta
+from typing import Any
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from app.users.user_schemas import User
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+ALGORITHM = "HS256"
+
+
+def create_access_token(subject: str | Any, expires_delta: timedelta) -> str:
+    expire = datetime.utcnow() + expires_delta
+    to_encode = {"exp": expire, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+def get_user_by_email(*, session: Session, email: str) -> User | None:
+    # statement = select(User).where(User.email == email)
+    # session_user = session.exec(statement).first()
+    return True
+
+
+def authenticate(*, session: Session, email: str, password: str) -> User | None:
+    db_user = get_user_by_email(session=session, email=email)
+    if not db_user:
+        return None
+    if not verify_password(password, db_user.hashed_password):
+        return None
+    return db_user
