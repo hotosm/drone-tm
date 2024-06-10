3190f78db49045f6e17c677304f08c584cc80404
Niraj Adhikari <nrjadkry@gmail.com>
2024-06-10 11:25:50 +0545

    update: user register schema validation

--- src/backend/app/users/user_schemas.py
+++ src/backend/app/users/user_schemas.py
@@ -1,17 +1,17 @@
-from pydantic import BaseModel
-from typing import Optional
+from pydantic import BaseModel, EmailStr, ValidationInfo, Field
+from pydantic.functional_validators import field_validator
 
 
 class UserBase(BaseModel):
     username: str
-    email_address: str
+    email_address: EmailStr
     is_active: bool = False
     is_superuser: bool = False
     name: str
 
 
 class User(BaseModel):
-    email_address: str
+    email_address: EmailStr
     is_active: bool = True
     is_superuser: bool = False
     name: str
@@ -22,19 +22,37 @@ class Token(BaseModel):
     token_type: str = "bearer"
 
 
-class UserPublic(BaseModel):
-    id: int
-    email_address: str
-    name: str
-    is_active: bool
-    is_superuser: bool
+class UserPublic(UserBase):
+    pass
 
 
 class UserRegister(BaseModel):
-    username: str
-    email_address: str
+    username: str = Field(min_length=4)
+    email_address: EmailStr
     password: str
-    name: Optional[str] = None
+    name: str
+
+    @field_validator("name", mode="before")
+    @classmethod
+    def not_empty(cls, v: str, info: ValidationInfo):
+        if v is not None and v == "":
+            raise ValueError(f"{info.field_name} cannot be empty")
+        return v
+
+    @field_validator("password", mode="before")
+    def password_complexity(cls, v: str, info: ValidationInfo):
+        if len(v) < 8:
+            raise ValueError("Password must be at least 8 characters long")
+        # TODO: Add them if required
+        # if not re.search(r'[A-Z]', v):
+        #     raise ValueError('Password must contain at least one uppercase letter')
+        # if not re.search(r'[a-z]', v):
+        #     raise ValueError('Password must contain at least one lowercase letter')
+        # if not re.search(r'[0-9]', v):
+        #     raise ValueError('Password must contain at least one digit')
+        # if not re.search(r'[!@#$%^&*(),.?":{}|<>]', v):
+        #     raise ValueError('Password must contain at least one special character')
+        return v
 
 
 class UserCreate(UserBase):
