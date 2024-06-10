15d33eb41fe6ff701224eff4d0143247616c9a95
Niraj Adhikari <nrjadkry@gmail.com>
2024-06-10 09:32:05 +0545

    signup endpoint

--- src/backend/app/users/user_schemas.py
+++ src/backend/app/users/user_schemas.py
@@ -1,13 +1,41 @@
 from pydantic import BaseModel
+from typing import Optional
+
+
+class UserBase(BaseModel):
+    username: str
+    email_address: str
+    is_active: bool = False
+    is_superuser: bool = False
+    name: str
 
 
 class User(BaseModel):
-    email: str
+    email_address: str
     is_active: bool = True
     is_superuser: bool = False
-    full_name: str
+    name: str
 
 
 class Token(BaseModel):
     access_token: str
     token_type: str = "bearer"
+
+
+class UserPublic(BaseModel):
+    id: int
+    email_address: str
+    name: str
+    is_active: bool
+    is_superuser: bool
+
+
+class UserRegister(BaseModel):
+    username: str
+    email_address: str
+    password: str
+    name: Optional[str] = None
+
+
+class UserCreate(UserBase):
+    password: str
