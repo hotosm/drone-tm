180fd8626aceccdf8a72a677b80050b91dd69bed
Niraj Adhikari <nrjadkry@gmail.com>
2024-06-10 09:28:47 +0545

    get user by username, email functions

--- src/backend/app/users/user_crud.py
+++ src/backend/app/users/user_crud.py
@@ -4,7 +4,10 @@ from datetime import datetime, timedelta
 from typing import Any
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
-from app.users.user_schemas import User
+from app.db import db_models
+from app.users.user_schemas import UserCreate
+from sqlalchemy import text
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -27,16 +30,46 @@ def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
 
 
-def get_user_by_email(*, session: Session, email: str) -> User | None:
-    # statement = select(User).where(User.email == email)
-    # session_user = session.exec(statement).first()
-    return True
+# def get_user_by_email(db: Session, email: str) -> db_models.DbUser | None:
+#     output = db.query(db_models.DbUser).filter(db_models.DbUser.email_address == email).first()
+#     print("output = ",output)
+#     return output
 
 
-def authenticate(*, session: Session, email: str, password: str) -> User | None:
-    db_user = get_user_by_email(session=session, email=email)
+def get_user_by_email(db: Session, email: str):
+    query = text(f"SELECT * FROM users WHERE email_address = '{email}' LIMIT 1;")
+    result = db.execute(query)
+    data = result.fetchone()
+    return data
+
+
+def get_user_by_username(db: Session, username: str):
+    query = text(f"SELECT * FROM users WHERE username = '{username}' LIMIT 1;")
+    result = db.execute(query)
+    data = result.fetchone()
+    return data
+
+
+# def get_user_by_username(db: Session, username: str):
+#     return db.query(db_models.DbUser).filter(db_models.DbUser.username==username).first()
+
+
+def authenticate(db: Session, email: str, password: str) -> db_models.DbUser | None:
+    db_user = get_user_by_username(db, email)
     if not db_user:
         return None
-    if not verify_password(password, db_user.hashed_password):
+    if not verify_password(password, db_user.password):
         return None
     return db_user
+
+
+def create_user(db: Session, user_create: UserCreate):
+    db_obj = db_models.DbUser(
+        password=get_password_hash(user_create.password),
+        **user_create.model_dump(exclude=["password"]),
+    )
+
+    db.add(db_obj)
+    db.commit()
+    db.refresh(db_obj)
+    return db_obj
