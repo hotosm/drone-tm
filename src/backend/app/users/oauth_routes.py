743447b43c0faccc9a67fb9af81374216bc44114
Niraj Adhikari <nrjadkry@gmail.com>
2024-06-23 16:19:27 +0545

    user_info endpoint which returns the auth user information

--- src/backend/app/users/oauth_routes.py
+++ src/backend/app/users/oauth_routes.py
@@ -1,9 +1,17 @@
+import os
 import json
 from loguru import logger as log
 from fastapi import Depends, Request
-from app.users.user_routes import router
 from fastapi.responses import JSONResponse
-from app.users.user_deps import init_google_auth
+from sqlalchemy.orm import Session
+from app.db import database
+from app.users.user_routes import router
+from app.users.user_deps import init_google_auth, login_required
+from app.users.user_schemas import AuthUser
+from app.config import settings
+
+if settings.DEBUG:
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
 
 @router.get("/google-login")
@@ -30,6 +38,16 @@ async def login_url(google_auth=Depends(init_google_auth)):
 async def callback(request: Request, google_auth=Depends(init_google_auth)):
     """Performs token exchange between Google and DTM API"""
 
-    access_token = google_auth.callback(str(request.url))
-
+    callback_url = str(request.url)
+    access_token = google_auth.callback(callback_url).get("access_token")
     return json.loads(access_token)
+
+
+@router.get("/my-info/")
+async def my_data(
+    db: Session = Depends(database.get_db),
+    user_data: AuthUser = Depends(login_required),
+):
+    """Read access token and get user details from Google"""
+
+    return user_data
