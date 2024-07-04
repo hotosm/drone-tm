14c4e09e152d96733959afe35f06dc73b163f976
Niraj Adhikari <nrjadkry@gmail.com>
2024-07-04 13:25:36 +0545

    user id type updated in AuthUser schema

--- src/backend/app/users/user_schemas.py
+++ src/backend/app/users/user_schemas.py
@@ -6,7 +6,7 @@ from typing import Optional
 class AuthUser(BaseModel):
     """The user model returned from Google OAuth2."""
 
-    id: int
+    id: str
     email: EmailStr
     img_url: Optional[str] = None
 
