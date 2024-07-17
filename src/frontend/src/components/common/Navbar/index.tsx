bc3d18fbcf7409cab97dcd69b321eb65604b404d
prajwalism <prajwalkhadgi11@gmail.com>
2024-07-17 17:44:56 +0545

    feat: change the logo in navbar to black

--- src/frontend/src/components/common/Navbar/index.tsx
+++ src/frontend/src/components/common/Navbar/index.tsx
@@ -1,6 +1,6 @@
 import Image from '@Components/RadixComponents/Image';
 import { NavLink } from 'react-router-dom';
-import dtmLogo from '@Assets/images/LandingPage/DTM-logo-red.svg';
+import dtmLogo from '@Assets/images/DTM-logo-black.svg';
 import { navLinks } from '@Constants/index';
 import UserProfile from '../UserProfile';
 import { FlexRow } from '../Layouts';
