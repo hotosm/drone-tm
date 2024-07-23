1cf94f247e2a38ddbf190cfa5be0b078c57e3afd
spwoodcock <sam.woodcock@protonmail.com>
2024-07-23 13:26:04 +0100

    build: improve code in pg-upgrade.sh bash script

--- contrib/pg-upgrade/upgrade-db.sh
+++ contrib/pg-upgrade/upgrade-db.sh
@@ -1,50 +1,37 @@
 #!/bin/bash
 
+set -e
+
+COMPOSE_FILES="-f docker-compose.yml -f contrib/pg-upgrade/docker-compose.yml"
+
+cleanup() {
+    echo "Cleaning up Docker containers..."
+    docker compose ${COMPOSE_FILES} down --remove-orphans
+    docker compose ${COMPOSE_FILES} rm --force db-check-upgrade || true
+}
+trap cleanup EXIT
+
 # Pull upgrade container
-docker compose \
-    -f docker-compose.yml \
-    -f contrib/pg-upgrade/docker-compose.yml \
-    pull db-upgrade-version
+docker compose ${COMPOSE_FILES} pull db-upgrade-version
 
 # Get exit code from db version check
-docker compose \
-    -f docker-compose.yml \
-    -f contrib/pg-upgrade/docker-compose.yml \
-    up db-check-upgrade --exit-code-from db-check-upgrade
+docker compose ${COMPOSE_FILES} up db-check-upgrade \
+    --exit-code-from db-check-upgrade \
+    --timeout 120
 exit_code=$?
+echo "db-check-upgrade exited with code $exit_code"
 
 # Exit script if upgrade complete
 if [ "$exit_code" -eq 1 ]; then
     echo "Database is already upgraded. Skipping."
-
-    docker compose \
-        -f docker-compose.yml \
-        -f contrib/pg-upgrade/docker-compose.yml \
-        rm --force db-check-upgrade
-
     exit 0
 fi
 
 # Stop any existing locks on db
-docker compose \
-    -f docker-compose.yml \
-    -f contrib/pg-upgrade/docker-compose.yml \
-    down
-
+docker compose ${COMPOSE_FILES} down
 # Do the db upgrade
-docker compose \
-    -f docker-compose.yml \
-    -f contrib/pg-upgrade/docker-compose.yml \
-    up -d db-upgrade
-
+docker compose ${COMPOSE_FILES} up -d db-upgrade
 # View any logs
-docker compose \
-    -f docker-compose.yml \
-    -f contrib/pg-upgrade/docker-compose.yml \
-    logs db-upgrade-version
-
-# Shut down db to prior to restart
-docker compose \
-    -f docker-compose.yml \
-    -f contrib/pg-upgrade/docker-compose.yml \
-    down
+docker compose ${COMPOSE_FILES} logs db-upgrade-version
+# Shut down db prior to restart
+docker compose ${COMPOSE_FILES} down
