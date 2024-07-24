6310af6a9867dbce90928d50d3c7271d18536383
prajwalism <prajwalkhadgi11@gmail.com>
2024-07-24 07:12:09 +0545

    fix: temporarily remove publish option in create project section

--- src/frontend/src/components/CreateProject/FormContents/Contributions/index.tsx
+++ src/frontend/src/components/CreateProject/FormContents/Contributions/index.tsx
@@ -1,24 +1,24 @@
-import { useTypedDispatch, useTypedSelector } from '@Store/hooks';
+// import { useTypedDispatch, useTypedSelector } from '@Store/hooks';
 import { FlexColumn } from '@Components/common/Layouts';
 import { FormControl, Label, Input } from '@Components/common/FormUI';
-import RadioButton from '@Components/common/RadioButton';
+// import RadioButton from '@Components/common/RadioButton';
 import ErrorMessage from '@Components/common/ErrorMessage';
 import { UseFormPropsType } from '@Components/common/FormUI/types';
-import { setCreateProjectState } from '@Store/actions/createproject';
-import { contributionsOptions } from '@Constants/createProject';
+// import { setCreateProjectState } from '@Store/actions/createproject';
+// import { contributionsOptions } from '@Constants/createProject';
 
 export default function Conditions({
   formProps,
 }: {
   formProps: UseFormPropsType;
 }) {
-  const dispatch = useTypedDispatch();
+  // const dispatch = useTypedDispatch();
 
   const { register, errors } = formProps;
 
-  const contributionsOption = useTypedSelector(
-    state => state.createproject.contributionsOption,
-  );
+  // const contributionsOption = useTypedSelector(
+  //   state => state.createproject.contributionsOption,
+  // );
 
   return (
     <FlexColumn gap={5} className="naxatw-px-10 naxatw-py-5">
@@ -34,7 +34,7 @@ export default function Conditions({
           message={errors?.per_task_instructions?.message as string}
         />
       </FormControl>
-      <RadioButton
+      {/* <RadioButton
         required
         topic="Publish"
         options={contributionsOptions}
@@ -43,7 +43,7 @@ export default function Conditions({
           dispatch(setCreateProjectState({ contributionsOption: val }));
         }}
         value={contributionsOption}
-      />
+      /> */}
       <FormControl className="naxatw-gap-1">
         <div className="naxatw-w-full">
           <Label required>Deadline for Submission</Label>
