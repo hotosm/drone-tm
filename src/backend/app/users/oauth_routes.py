79d334720dd1800687f3c5f8da054ea29ff78b1e
Niraj Adhikari <nrjadkry@gmail.com>
2024-07-24 11:11:12 +0545

    fix: enforce https callback url

--- src/backend/app/users/oauth_routes.py
+++ src/backend/app/users/oauth_routes.py
@@ -39,7 +39,9 @@ async def login_url(google_auth=Depends(init_google_auth)):
 async def callback(request: Request, google_auth=Depends(init_google_auth)):
     """Performs token exchange between Google and DTM API"""
 
-    callback_url = str(request.url)
+    # Enforce https callback url
+    callback_url = str(request.url).replace("http://", "https://")
+
     access_token = google_auth.callback(callback_url).get("access_token")
 
     user_data = google_auth.deserialize_access_token(access_token)
