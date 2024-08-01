dd8047c29d2baff7bb87e2f556669763c20fe4fa
Sujit <sujit.naxa@gmail.com>
2024-08-01 15:37:03 +0545

    feat(dashboard): update cards UI to show active

--- src/frontend/src/constants/dashboard.ts
+++ src/frontend/src/constants/dashboard.ts
@@ -1,10 +1,28 @@
 /* eslint-disable import/prefer-default-export */
-export const dashboardCards = [
+export const dashboardCardsForProjectCreator = [
   {
     id: 1,
-    title: 'Task Created',
+    title: 'Request Logs',
     value: 12,
   },
+  {
+    id: 2,
+    title: 'Ongoing Task',
+    value: 7,
+  },
+  {
+    id: 3,
+    title: 'Completed Task',
+    value: 5,
+  },
+  {
+    id: 4,
+    title: 'Completed Task',
+    value: 7,
+  },
+];
+
+export const dashboardCardsForDroneOperator = [
   {
     id: 2,
     title: 'Ongoing Task',
