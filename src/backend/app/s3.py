66cf12028b177e935b799258b1ff370f997e6e96
Niraj Adhikari <nrjadkry@gmail.com>
2024-08-06 15:37:20 +0545

    minio utility functions

--- src/backend/app/s3.py
+++ src/backend/app/s3.py
@@ -1,6 +1,7 @@
 from app.config import settings
 from loguru import logger as log
 from minio import Minio
+from io import BytesIO
 
 
 def s3_client():
@@ -35,3 +36,63 @@ def is_connection_secure(minio_url: str):
         raise ValueError(err)
 
     return stripped_url, secure
+
+
+def add_file_to_bucket(bucket_name: str, file_path: str, s3_path: str):
+    """Upload a file from the filesystem to an S3 bucket.
+
+    Args:
+        bucket_name (str): The name of the S3 bucket.
+        file_path (str): The path to the file on the local filesystem.
+        s3_path (str): The path in the S3 bucket where the file will be stored.
+    """
+    client = s3_client()
+    client.fput_object(bucket_name, file_path, s3_path)
+
+
+def add_obj_to_bucket(bucket_name: str, file_obj: BytesIO, s3_path: str):
+    """Upload a BytesIO object to an S3 bucket.
+
+    Args:
+        bucket_name (str): The name of the S3 bucket.
+        file_obj (BytesIO): A BytesIO object containing the data to be uploaded.
+        s3_path (str): The path in the S3 bucket where the data will be stored.
+    """
+    client = s3_client()
+    result = client.put_object(bucket_name, file_obj, s3_path)
+    log.debug(
+        f"Created {result.object_name} object; etag: {result.etag}, "
+        f"version-id: {result.version_id}"
+    )
+
+
+def get_file_from_bucket(bucket_name: str, s3_path: str, file_path: str):
+    """Download a file from an S3 bucket and save it to the local filesystem.
+
+    Args:
+        bucket_name (str): The name of the S3 bucket.
+        s3_path (str): The path to the file in the S3 bucket.
+        file_path (str): The path on the local filesystem where the S3
+            file will be saved.
+    """
+    client = s3_client()
+    client.fget_object(bucket_name, s3_path, file_path)
+
+
+def get_obj_from_bucket(bucket_name: str, s3_path: str) -> BytesIO:
+    """Download an S3 object from a bucket and return it as a BytesIO object.
+
+    Args:
+        bucket_name (str): The name of the S3 bucket.
+        s3_path (str): The path to the S3 object in the bucket.
+
+    Returns:
+        BytesIO: A BytesIO object containing the content of the downloaded S3 object.
+    """
+    client = s3_client()
+    try:
+        response = client.get_object(bucket_name, s3_path)
+        return BytesIO(response.read())
+    finally:
+        response.close()
+        response.release_conn()
