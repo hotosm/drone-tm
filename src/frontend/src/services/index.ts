4291decaa77d7a8e860928fc9031216f89540c73
Sujit <sujit.naxa@gmail.com>
2024-08-07 11:25:51 +0545

    feat(services): add request and response interceptors to handle access and refresh token expiry

--- src/frontend/src/services/index.ts
+++ src/frontend/src/services/index.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-param-reassign */
 import axios, { AxiosInstance } from 'axios';
+import { toast } from 'react-toastify';
 
 const { API_URL_V1, BASE_URL } = process.env;
 
@@ -14,6 +15,53 @@ export const api = axios.create({
     'Content-Type': 'multipart/form-data',
   },
 });
+
+// This interceptor is required to set token on request
+function requestInterceptorFunction(config: any): any {
+  const token = localStorage.getItem('token');
+  // eslint-disable-next-line no-param-reassign
+  config.headers['Access-Token'] = `${token}`;
+  return config;
+}
+
+api.interceptors.request.use(requestInterceptorFunction);
+api.interceptors.response.use(
+  response => response,
+  async responseError => {
+    // handle token expire or invalid token case
+    const originalRequest = responseError.config;
+    if (
+      responseError.response.status === 401 &&
+      responseError.response.data.detail === 'Access token not valid' &&
+      // eslint-disable-next-line no-underscore-dangle
+      !originalRequest._retry
+    ) {
+      // eslint-disable-next-line no-underscore-dangle
+      originalRequest._retry = true;
+      const refreshToken = localStorage.getItem('refresh');
+
+      if (refreshToken) {
+        try {
+          const response = await axios.get(`${BASE_URL}/users/refresh-token`, {
+            headers: { 'access-token': refreshToken },
+          });
+          const newAccessToken = response.data.access_token;
+          localStorage.setItem('token', newAccessToken); // set new access token
+          originalRequest.headers['Access-Token'] = `${newAccessToken}`;
+          return axios(originalRequest); // recall Api with new token1
+        } catch (error: any) {
+          toast.error('Session Expired. Please Re-login.');
+          localStorage.removeItem('token');
+          localStorage.removeItem('refresh');
+          window.location.href = '/';
+          return Promise.reject(responseError);
+        }
+      }
+    }
+    // eslint-disable-next-line prefer-promise-reject-errors
+    return Promise.reject({ ...responseError });
+  },
+);
 
 export const authenticated = (apiInstance: AxiosInstance) => {
   const token = localStorage.getItem('token');
