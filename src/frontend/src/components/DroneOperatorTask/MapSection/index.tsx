a98aa1e69c4ef0754c878e53e1d6805fdc42c08a
Bijay Rauniyar <rauniyarbijay0@gmail.com>
2024-08-09 12:03:14 +0000

    feat: add componet for map section

---
+++ src/frontend/src/components/DroneOperatorTask/MapSection/index.tsx
@@ -0,0 +1,9 @@
+const DroneOperatorMapBox = () => {
+  return (
+    <>
+      <div className="naxatw-h-[70vh] naxatw-w-full naxatw-rounded-xl naxatw-bg-gray-200" />
+    </>
+  );
+};
+
+export default DroneOperatorMapBox;
