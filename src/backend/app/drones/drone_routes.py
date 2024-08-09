326da39a4b6acd9f4a21c797efc059842b3781b3
Niraj Adhikari <nrjadkry@gmail.com>
2024-08-09 17:13:45 +0545

    drone crud with psycopg and pydantic schemas

--- src/backend/app/drones/drone_routes.py
+++ src/backend/app/drones/drone_routes.py
@@ -8,35 +8,38 @@ from app.config import settings
 from app.drones import drone_schemas
 from psycopg import Connection
 from app.drones import drone_crud
-from typing import List
 
 
 router = APIRouter(
     prefix=f"{settings.API_PREFIX}/drones",
+    tags=["Drones"],
     responses={404: {"description": "Not found"}},
 )
 
 
-@router.get("/", tags=["Drones"], response_model=List[drone_schemas.DroneOut])
+@router.get("/", response_model=list[drone_schemas.DroneOut])
 async def read_drones(
+    db: Annotated[Connection, Depends(database.get_db)],
+):
+    """Get all drones."""
+    try:
+        return await drone_schemas.DbDrone.all(db)
+    except KeyError as e:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND) from e
+
+
+@router.post("/create_drone")
+async def create_drone(
+    drone_info: drone_schemas.DroneIn,
     db: Annotated[Connection, Depends(database.get_db)],
     user_data: Annotated[AuthUser, Depends(login_required)],
 ):
-    """
-    Retrieves all drone records from the database.
-
-    Args:
-        db (Database, optional): The database session object.
-        user_data (AuthUser, optional): The authenticated user data.
-
-    Returns:
-        List[drone_schemas.DroneOut]: A list of all drone records.
-    """
-    drones = await drone_crud.read_all_drones(db)
-    return drones
+    """Create a new drone in database"""
+    drone_id = await drone_schemas.DbDrone.create(db, drone_info)
+    return {"message": "Drone created successfully", "drone_id": drone_id}
 
 
-@router.delete("/{drone_id}", tags=["Drones"])
+@router.delete("/{drone_id}")
 async def delete_drone(
     drone_id: int,
     db: Annotated[Connection, Depends(database.get_db)],
@@ -53,35 +56,13 @@ async def delete_drone(
     Returns:
         dict: A success message if the drone was deleted.
     """
+
+    # TODO: Check user role, Admin can only do this
+
     success = await drone_crud.delete_drone(db, drone_id)
     if not success:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Drone not found")
     return {"message": "Drone deleted successfully"}
-
-
-@router.post("/create_drone", tags=["Drones"])
-async def create_drone(
-    drone_info: drone_schemas.DroneIn,
-    db: Annotated[Connection, Depends(database.get_db)],
-    user_data: Annotated[AuthUser, Depends(login_required)],
-):
-    """
-    Creates a new drone record in the database.
-
-    Args:
-        drone_info (drone_schemas.DroneIn): The schema object containing drone details.
-        db (Database, optional): The database session object.
-        user_data (AuthUser, optional): The authenticated user data.
-
-    Returns:
-        dict: A dictionary containing a success message and the ID of the newly created drone.
-    """
-    drone_id = await drone_crud.create_drone(db, drone_info)
-    if not drone_id:
-        raise HTTPException(
-            status_code=HTTPStatus.BAD_REQUEST, detail="Drone creation failed"
-        )
-    return {"message": "Drone created successfully", "drone_id": drone_id}
 
 
 @router.get("/{drone_id}", tags=["Drones"], response_model=drone_schemas.DroneOut)
