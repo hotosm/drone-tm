7564a35903d063a5a935628e0c712eada7784ef4
Bijay Rauniyar <rauniyarbijay0@gmail.com>
2024-08-09 11:59:02 +0000

    feat: add drone operator task page

---
+++ src/frontend/src/components/DroneOperatorTask/index.tsx
@@ -0,0 +1,21 @@
+import DroneOperatorTaskHeader from './Header';
+import DroneOperatorDescriptionBox from './DescriptionBox';
+import DroneOperatorMapBox from './MapSection';
+
+const DroneOperatorTaskComponent = () => {
+  return (
+    <>
+      <div className="naxatw-mx-auto naxatw-w-11/12 naxatw-max-w-[90rem] naxatw-px-8 naxatw-pb-[2.9375rem] naxatw-pt-3">
+        <div className="naxatw-flex naxatw-flex-col naxatw-gap-3">
+          <DroneOperatorTaskHeader />
+          <div className="naxatw-grid naxatw-grid-cols-[30%_70%] naxatw-gap-5">
+            <DroneOperatorDescriptionBox />
+            <DroneOperatorMapBox />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DroneOperatorTaskComponent;
---
+++ src/frontend/src/views/DroneOperatorTask/index.tsx
@@ -0,0 +1,13 @@
+import DroneOperatorTaskComponent from '@Components/DroneOperatorTask';
+
+const DroneOperartorTaskPage = () => {
+  return (
+    <>
+      <div className="naxatw-min-h-[calc(100vh-99px)]">
+        <DroneOperatorTaskComponent />
+      </div>
+    </>
+  );
+};
+
+export default DroneOperartorTaskPage;
