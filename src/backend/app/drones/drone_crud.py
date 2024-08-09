52c863e15c6a12ccc5c46fb41754b8c4ef75dc18
Niraj Adhikari <nrjadkry@gmail.com>
2024-08-09 17:17:17 +0545

    remove create_drone function from drones

--- src/backend/app/drones/drone_crud.py
+++ src/backend/app/drones/drone_crud.py
@@ -1,4 +1,3 @@
-from app.drones import drone_schemas
 from app.models.enums import HTTPStatus
 from loguru import logger as log
 from fastapi import HTTPException
@@ -87,45 +86,4 @@ async def get_drone(db: Connection, drone_id: int):
         log.exception(e)
         raise HTTPException(
             status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail="Retrieval failed"
-        ) from e
-
-
-async def create_drone(db: Connection, drone_info: drone_schemas.DroneIn):
-    """
-    Creates a new drone record in the database.
-
-    Args:
-        db (Database): The database connection object.
-        drone (drone_schemas.DroneIn): The schema object containing drone details.
-
-    Returns:
-        The ID of the newly created drone record.
-    """
-    try:
-        insert_query = """
-            INSERT INTO drones (
-                model, manufacturer, camera_model, sensor_width, sensor_height,
-                max_battery_health, focal_length, image_width, image_height,
-                max_altitude, max_speed, weight, created
-            ) VALUES (
-                :model, :manufacturer, :camera_model, :sensor_width, :sensor_height,
-                :max_battery_health, :focal_length, :image_width, :image_height,
-                :max_altitude, :max_speed, :weight, CURRENT_TIMESTAMP
-            )
-            RETURNING id
-        """
-        result = await db.execute(insert_query, drone_info.__dict__)
-        return result
-
-    # except UniqueViolationError as e:
-    #     log.exception("Unique constraint violation: %s", e)
-    #     raise HTTPException(
-    #         status_code=HTTPStatus.CONFLICT,
-    #         detail="A drone with this model already exists",
-    #     )
-
-    except Exception as e:
-        log.exception(e)
-        raise HTTPException(
-            status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail="Drone creation failed"
         ) from e
