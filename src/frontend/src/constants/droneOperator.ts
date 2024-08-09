0d0e38307ffc25639340bbe7eeb07fbf63ffb8d9
Bijay Rauniyar <rauniyarbijay0@gmail.com>
2024-08-09 12:01:49 +0000

    feat: add constants for drone operator task page

---
+++ src/frontend/src/constants/droneOperator.ts
@@ -0,0 +1,31 @@
+export const descriptionData = [
+  [
+    {
+      name: 'Total task area',
+      value: '4.75 km2',
+    },
+    {
+      name: 'Est. flight time',
+      value: '20 minutes',
+    },
+  ],
+  [
+    {
+      name: 'Altitude',
+      value: '50 meter',
+    },
+    {
+      name: 'Gimble Angle',
+      value: '-90 degree',
+    },
+    {
+      name: 'Image Overlap',
+      value: '70',
+    },
+    {
+      name: 'Starting Point Altitude',
+      value: '50 meter',
+    },
+  ],
+];
+export const descriptionTitle = ['Task Description', 'Flight Parameters'];
