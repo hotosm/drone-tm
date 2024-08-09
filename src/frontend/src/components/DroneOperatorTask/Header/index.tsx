b577d7de20be36021491ae5e9f5e17fbcff264b6
Bijay Rauniyar <rauniyarbijay0@gmail.com>
2024-08-09 12:01:14 +0000

    feat: add drone operator task header

---
+++ src/frontend/src/components/DroneOperatorTask/Header/index.tsx
@@ -0,0 +1,21 @@
+const DroneOperatorTaskHeader = () => {
+  return (
+    <>
+      <div className="naxatw-self-stretch naxatw-py-3">
+        <div className="naxatw-flex naxatw-items-center naxatw-gap-1">
+          <p className="naxatw-text-sm naxatw-font-normal naxatw-leading-normal naxatw-tracking-[0.0175rem] naxatw-text-[#212121]">
+            Dashboard
+          </p>
+          <p className="naxatw-text-sm naxatw-font-normal naxatw-leading-normal naxatw-tracking-[0.0175rem] naxatw-text-[#212121]">
+            /
+          </p>
+          <p className="naxatw-text-sm naxatw-font-semibold naxatw-leading-normal naxatw-tracking-[0.0175rem] naxatw-text-[#212121]">
+            St. Lucia Damage Assessment Drone Survey
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DroneOperatorTaskHeader;
