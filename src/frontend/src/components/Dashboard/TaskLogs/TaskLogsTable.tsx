bc561bfd6a9911a90212bf84c2d9f3c3a9be307d
Sujit <sujit.naxa@gmail.com>
2024-08-12 09:34:50 +0545

    feat(dashbaord): add padding on task listing table row

--- src/frontend/src/components/Dashboard/TaskLogs/TaskLogsTable.tsx
+++ src/frontend/src/components/Dashboard/TaskLogs/TaskLogsTable.tsx
@@ -33,12 +33,14 @@ const TaskLogsTable = ({ data: taskList }: ITaskLogsTableProps) => {
         <tbody>
           {taskList?.map(task => (
             <tr key={task.task_id}>
-              <td>{task.task_id}</td>
-              <td>{task.task_area}</td>
-              <td>-</td>
-              <td>{format(new Date(task.created_at), 'yyyy-MM-dd')}</td>
-              <td>{task.state}</td>
-              <td className="naxatw-flex naxatw-items-center">
+              <td className="naxatw-px-2 naxatw-py-1">{task.task_id}</td>
+              <td className="naxatw-px-2 naxatw-py-1">{task.task_area}</td>
+              <td className="naxatw-px-2 naxatw-py-1">-</td>
+              <td className="naxatw-px-2 naxatw-py-1">
+                {format(new Date(task.created_at), 'yyyy-MM-dd')}
+              </td>
+              <td className="naxatw-px-2">{task.state}</td>
+              <td className="naxatw-flex naxatw-items-center naxatw-px-2">
                 <div
                   className="naxatw-flex naxatw-h-8 naxatw-w-8 naxatw-cursor-pointer naxatw-items-center naxatw-justify-center naxatw-rounded-lg hover:naxatw-bg-gray-200"
                   role="presentation"
