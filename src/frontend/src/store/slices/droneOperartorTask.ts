2e685b1eb85ad657246bf4d5336e591e4fde814d
Bijay Rauniyar <rauniyarbijay0@gmail.com>
2024-08-14 08:59:17 +0000

    feat: add action and reducer for droneOperatorTasks

--- src/frontend/src/store/slices/droneOperartorTask.ts
+++ src/frontend/src/store/slices/droneOperartorTask.ts
@@ -4,11 +4,17 @@ import { createSlice } from '@reduxjs/toolkit';
 export interface IDroneOperatorTaskState {
   secondPage: boolean;
   secondPageState: string;
+  clickedImage: string;
+  checkedImages: Record<number, boolean>;
+  popOver: boolean;
 }
 
 const initialState: IDroneOperatorTaskState = {
   secondPage: false,
   secondPageState: 'description',
+  clickedImage: '',
+  checkedImages: {},
+  popOver: false,
 };
 
 export const droneOperatorTaskSlice = createSlice({
@@ -20,6 +26,29 @@ export const droneOperatorTaskSlice = createSlice({
     },
     setSecondPageState: (state, action) => {
       state.secondPageState = action.payload;
+    },
+    setSelectedImage: (state, action) => {
+      state.clickedImage = action.payload;
+    },
+    setCheckedImages: (state, action) => {
+      state.checkedImages = action.payload;
+    },
+    unCheckImages: (state, action) => {
+      state.checkedImages[action.payload] =
+        !state.checkedImages[action.payload];
+    },
+    showPopover: state => {
+      state.popOver = !state.popOver;
+    },
+    unCheckAllImages: state => {
+      Object.keys(state.checkedImages).forEach((key: any) => {
+        state.checkedImages[key] = false;
+      });
+    },
+    checkAllImages: state => {
+      Object.keys(state.checkedImages).forEach((key: any) => {
+        state.checkedImages[key] = true;
+      });
     },
   },
 });
