815dcc7807ce69eda0c17b07bbcb5bbaa9ebbd9a
Sujit <sujit.naxa@gmail.com>
2024-08-15 11:52:06 +0545

    fix: type issue on IRoute
    add type to accept component as a prop and return new jsx element `((props: { children?: ReactNode }) => JSX.Element)`

--- src/frontend/src/routes/types.ts
+++ src/frontend/src/routes/types.ts
@@ -1,10 +1,16 @@
-import { LazyExoticComponent } from 'react';
+import { LazyExoticComponent, ReactNode } from 'react';
 
 export interface IRoute {
   id?: number;
   path: string;
   name: string;
-  component: LazyExoticComponent<() => JSX.Element> | (() => JSX.Element);
+  component:
+    | LazyExoticComponent<() => JSX.Element>
+    | (() => JSX.Element)
+    // eslint-disable-next-line no-unused-vars
+    | ((props: { children?: ReactNode }) => JSX.Element)
+    | (() => JSX.Element)
+    | (() => JSX.Element);
   authenticated?: boolean;
   children?: IRoute[];
 }
