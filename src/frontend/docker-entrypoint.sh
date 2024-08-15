022622959fff0dc14556b50d68cf482025de0154
Nischal Shrestha <nischalstha9@gmail.com>
2024-08-15 14:36:12 +0545

    fix: frontend html copy to backend migrate from docker socket to docker volume

--- src/frontend/docker-entrypoint.sh
+++ src/frontend/docker-entrypoint.sh
@@ -10,7 +10,7 @@ if [[ -z "${S3_SECRET_KEY}" ]]; then
   echo "Missing environment variable S3_SECRET_KEY"
   exit 0
 fi
-
+``
 MINIO_HOST_URL=${MINIO_HOST_URL:-http://localhost:9000}
 FRONTEND_BUCKET_NAME=${FRONTEND_BUCKET_NAME:-frontendstatic}
 
@@ -18,16 +18,15 @@ if [[ "${MINIO_HOST_URL}" == 'http://localhost:9000' ]]; then
   CONFIG_MINIO=http://minio:9000
 fi
 
-mcli config host add minio ${CONFIG_MINIO:-MINIO_HOST_URL} $S3_ACCESS_KEY $S3_SECRET_KEY
+mc config host add minio ${CONFIG_MINIO:-MINIO_HOST_URL} $S3_ACCESS_KEY $S3_SECRET_KEY
 
-mcli mb minio/${FRONTEND_BUCKET_NAME} || echo "Failed to create bucket in MinIO"
-mcli anonymous set download minio/${FRONTEND_BUCKET_NAME} || echo "Failed setting staticfiles dir as public in MinIO"
+mc mb minio/${FRONTEND_BUCKET_NAME} || echo "Failed to create bucket in MinIO"
+mc anonymous set download minio/${FRONTEND_BUCKET_NAME} || echo "Failed setting staticfiles dir as public in MinIO"
 
-mcli cp --recursive /tmp/dist/* minio/${FRONTEND_BUCKET_NAME}
+mc cp --recursive /tmp/dist/* minio/${FRONTEND_BUCKET_NAME}
 
-for backend_container in $(docker ps -a -q --filter "label=com.docker.compose.service=backend"); do
-  docker cp /tmp/dist/index.html $backend_container:/project/src/backend/templates/index.html
-done
+echo ========== Copying index.html to volume ==========
+cp -v /tmp/dist/index.html /tmp/frontend_html/index.html
 
 echo "======================================================"
 echo "           Index File Copied Successfully!            "
