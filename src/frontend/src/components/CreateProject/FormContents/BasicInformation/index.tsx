cc807fc6d89f8326e699d171b2403492e1898ef3
Sujit <sujit.naxa@gmail.com>
2024-08-22 10:47:14 +0545

    feat(project-creation): make project description input a text area

--- src/frontend/src/components/CreateProject/FormContents/BasicInformation/index.tsx
+++ src/frontend/src/components/CreateProject/FormContents/BasicInformation/index.tsx
@@ -1,13 +1,14 @@
 import { FormControl, Label, Input } from '@Components/common/FormUI';
 import ErrorMessage from '@Components/common/FormUI/ErrorMessage';
 import { UseFormPropsType } from '@Components/common/FormUI/types';
+import { Controller } from 'react-hook-form';
 
 export default function BasicInformation({
   formProps,
 }: {
   formProps: UseFormPropsType;
 }) {
-  const { register, errors } = formProps;
+  const { register, errors, control } = formProps;
 
   return (
     <div className="naxatw-px-10 naxatw-py-5">
@@ -23,14 +24,18 @@ export default function BasicInformation({
         <ErrorMessage message={errors?.name?.message as string} />
       </FormControl>
       <FormControl className="naxatw-mt-5 naxatw-gap-1">
-        <Label required>Description of the project</Label>
-        <Input
-          type="text-area"
-          placeholder="Description of the Project"
-          {...register('description', {
-            required: 'Description is Required',
-            setValueAs: (value: string) => value.trim(),
-          })}
+        <Label>Description of the project</Label>
+        <Controller
+          control={control}
+          name="description"
+          render={({ field: { onChange, onBlur, value, ref, ...others } }) => (
+            <textarea
+              className="naxatw-flex naxatw-h-[100px] naxatw-rounded-[4px] naxatw-border naxatw-border-[#555555] naxatw-bg-transparent naxatw-p-2 naxatw-text-body-md file:naxatw-font-medium hover:naxatw-border-red focus:naxatw-border-red focus:naxatw-bg-transparent focus:naxatw-outline-none disabled:naxatw-cursor-not-allowed"
+              placeholder="Description of the Project"
+              ref={ref}
+              {...others}
+            />
+          )}
         />
         <ErrorMessage message={errors?.description?.message as string} />
       </FormControl>
