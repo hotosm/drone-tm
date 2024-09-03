120454864349fc24e9e48a38cb5a9ce63abe9c3f
Sujit <sujit.naxa@gmail.com>
2024-09-03 15:42:40 +0545

    feat: add `LocateTheUser` component to redirect user to their current location

---
+++ src/frontend/src/components/common/MapLibreComponents/LocateUser/index.tsx
@@ -0,0 +1,27 @@
+import { GeolocateControl } from 'maplibre-gl';
+import { useEffect } from 'react';
+import { MapInstanceType } from '../types';
+
+interface ILocateTheUserProps {
+  map?: MapInstanceType;
+  isMapLoaded: Boolean;
+}
+
+const LocateUser = ({ map, isMapLoaded }: ILocateTheUserProps) => {
+  useEffect(() => {
+    if (!map || !isMapLoaded) return;
+    // Add geolocate control to the map.
+    map.addControl(
+      new GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        trackUserLocation: true,
+      }),
+    );
+  }, [map, isMapLoaded]);
+
+  return null;
+};
+
+export default LocateUser;
