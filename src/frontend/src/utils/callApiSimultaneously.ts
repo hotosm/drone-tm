ab22820633f1026c86702bde77b7e0fc30f18589
Sujit <sujitkarki703@gmail.com>
2024-09-30 13:28:09 +0545

    feat(task-description-image-upload): implement `retryFc` to retry the api in case of upload failure after 1 sec

--- src/frontend/src/utils/callApiSimultaneously.ts
+++ src/frontend/src/utils/callApiSimultaneously.ts
@@ -1,10 +1,31 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 // function that calls the api simultaneously
 export default async function callApiSimultaneously(urls: any, data: any) {
-  const promises = urls.map((url: any, index: any) =>
-    axios.put(url, data[index]),
+  // eslint-disable-next-line no-promise-executor-return
+  const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+  const retryFc = async (url: string, singleData: any, n: number) => {
+    try {
+      return await axios.put(url, singleData);
+    } catch (err) {
+      if (n === 1) throw err;
+      delay(1000); // 1 sec delay
+      // eslint-disable-next-line no-return-await
+      return await retryFc(url, singleData, n - 1);
+    }
+  };
+
+  const promises = urls.map(
+    (url: any, index: any) => retryFc(url, data[index], 3), // 3 entries for each api call
   );
-  const responses = await Promise.all(promises);
-  return responses;
+
+  try {
+    const responses = await Promise.all(promises);
+    return responses;
+  } catch (err) {
+    toast.error('Error occurred on image upload');
+    throw err;
+  }
 }
