ae7fba71b8fd0368b56a073257c30723ae737117
Sujit <sujitkarki703@gmail.com>
2024-10-02 11:34:12 +0545

    feat(update-profile): add OrganizationDetail component

---
+++ src/frontend/src/components/UpdateUserDetails/OrganizationDetails/index.tsx
@@ -0,0 +1,84 @@
+import ErrorMessage from '@Components/common/ErrorMessage';
+import { FormControl, Input, Label } from '@Components/common/FormUI';
+import { Flex, FlexColumn } from '@Components/common/Layouts';
+import { Button } from '@Components/RadixComponents/Button';
+import { getLocalStorageValue } from '@Utils/getLocalStorageValue';
+import { useForm } from 'react-hook-form';
+
+const OrganizationDetails = () => {
+  const userProfile = getLocalStorageValue('userprofile');
+
+  const initialState = {
+    organization_name: userProfile?.organization_name || null,
+    organization_address: userProfile?.organization_address || null,
+    job_title: userProfile?.job_title || null,
+  };
+
+  const { register, handleSubmit, formState } = useForm({
+    defaultValues: initialState,
+  });
+
+  return (
+    <section className="naxatw-w-full naxatw-px-14">
+      <Flex>
+        <p className="naxatw-py-2 naxatw-text-lg naxatw-font-bold">
+          Organization Details
+        </p>
+      </Flex>
+      <FlexColumn gap={5}>
+        <FormControl>
+          <Label>Organization Name</Label>
+          <Input
+            placeholder="Enter Organization Name"
+            className="naxatw-mt-1"
+            {...register('organization_name', {
+              // required: 'Organization name is Required',
+            })}
+          />
+          <ErrorMessage
+            message={formState.errors?.organization_name?.message as string}
+          />
+        </FormControl>
+        <FormControl>
+          <Label>Organization Address</Label>
+          <Input
+            placeholder="Enter Organization Address"
+            className="naxatw-mt-1"
+            {...register('organization_address', {
+              // required: 'Organization Address is Required',
+            })}
+          />
+          <ErrorMessage
+            message={formState.errors?.organization_address?.message as string}
+          />
+        </FormControl>
+        <FormControl>
+          <Label>Job Title</Label>
+          <Input
+            placeholder="Enter Job Title"
+            className="naxatw-mt-1"
+            {...register('job_title', {
+              // required: 'Job Title is Required',
+            })}
+          />
+          <ErrorMessage
+            message={formState.errors?.job_title?.message as string}
+          />
+        </FormControl>
+      </FlexColumn>
+      <div className="naxatw-flex naxatw-justify-center naxatw-py-4">
+        <Button
+          className="naxatw-bg-red"
+          onClick={e => {
+            e.preventDefault();
+          }}
+          withLoader
+        >
+          Save
+        </Button>
+      </div>
+    </section>
+  );
+};
+
+export default OrganizationDetails;
