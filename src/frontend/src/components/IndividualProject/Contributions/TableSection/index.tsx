55c86d202b1095e12fd76b977148577b732e4dfd
Sujit <sujitkarki703@gmail.com>
2024-10-04 17:40:19 +0545

    fix(individual-project): download orhophoto

--- src/frontend/src/components/IndividualProject/Contributions/TableSection/index.tsx
+++ src/frontend/src/components/IndividualProject/Contributions/TableSection/index.tsx
@@ -1,6 +1,8 @@
 import DataTable from '@Components/common/DataTable';
+import Icon from '@Components/common/Icon';
 import { useTypedSelector } from '@Store/hooks';
 import { useMemo } from 'react';
+import { toast } from 'react-toastify';
 
 const contributionsDataColumns = [
   {
@@ -14,6 +16,41 @@ const contributionsDataColumns = [
   {
     header: 'Task Status',
     accessorKey: 'task_state',
+  },
+  { header: 'Image count', accessorKey: 'image_count' },
+
+  {
+    header: 'Orthophoto',
+    accessorKey: 'assets_url',
+    cell: ({ row }: any) => {
+      const handleDownloadResult = () => {
+        const { original: rowData } = row;
+        if (!rowData?.assets_url) return;
+        try {
+          const link = document.createElement('a');
+          link.href = rowData?.assets_url;
+          link.download = 'assets.zip';
+          document.body.appendChild(link);
+          link.click();
+          link.remove();
+        } catch (error) {
+          toast.error(`There wan an error while downloading file ${error}`);
+        }
+      };
+
+      return (
+        <div
+          className="naxatw-group naxatw-flex naxatw-cursor-pointer naxatw-items-center naxatw-gap-1 naxatw-text-center naxatw-font-semibold naxatw-text-red"
+          tabIndex={0}
+          role="button"
+          onKeyDown={() => {}}
+          onClick={() => handleDownloadResult()}
+        >
+          <div className="group-hover:naxatw-underline">Download</div>
+          <Icon className="!naxatw-text-icon-sm" name="download" />
+        </div>
+      );
+    },
   },
 ];
 
@@ -30,6 +67,8 @@ export default function TableSection() {
           user: curr?.name || '-',
           task_mapped: `Task# ${curr?.project_task_index}`,
           task_state: curr?.state,
+          assets_url: curr?.assets_url,
+          image_count: curr?.image_count,
         },
       ];
     }, []);
