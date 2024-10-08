3740f2f599aba1f90b4134cf0c2be35f510cf430
Sujit <sujitkarki703@gmail.com>
2024-10-08 13:28:00 +0545

    feat(dashboard): update UI and pass other project details on project card props

--- src/frontend/src/views/Projects/index.tsx
+++ src/frontend/src/views/Projects/index.tsx
@@ -33,8 +33,8 @@ const Projects = () => {
       <ProjectsHeader />
       <div className="naxatw-grid naxatw-gap-2 md:naxatw-flex md:naxatw-h-[calc(100vh-8.5rem)]">
         <div
-          className={`scrollbar naxatw-grid naxatw-grid-rows-[16rem] naxatw-gap-3 naxatw-overflow-y-auto naxatw-py-2 ${showMap ? 'naxatw-w-full naxatw-grid-cols-1 md:naxatw-w-1/2 md:naxatw-grid-cols-2 lg:naxatw-grid-cols-3' : 'naxatw-w-full naxatw-grid-cols-1 sm:naxatw-grid-cols-2 md:naxatw-grid-cols-4 lg:naxatw-grid-cols-6'}`}
-          style={{ gridAutoRows: '16rem' }}
+          className={`scrollbar naxatw-grid naxatw-grid-rows-[19rem] naxatw-gap-3 naxatw-overflow-y-auto naxatw-py-2 ${showMap ? 'naxatw-w-full naxatw-grid-cols-1 md:naxatw-w-1/3 md:naxatw-grid-cols-1 lg:naxatw-grid-cols-1 xl:naxatw-grid-cols-2' : 'naxatw-w-full naxatw-grid-cols-1 sm:naxatw-grid-cols-2 md:naxatw-grid-cols-3 lg:naxatw-grid-cols-5'}`}
+          style={{ gridAutoRows: '19rem' }}
         >
           {isLoading ? (
             <>
@@ -50,9 +50,12 @@ const Projects = () => {
                   <ProjectCard
                     key={project.id}
                     id={project.id}
+                    slug={project.slug}
                     imageUrl={project?.image_url}
                     title={project.name}
                     description={project.description}
+                    totalTasks={project?.total_task_count}
+                    ongoingTasks={project?.ongoing_task_count}
                   />
                 ),
               )}
@@ -60,7 +63,7 @@ const Projects = () => {
           )}
         </div>
         {showMap && (
-          <div className="naxatw-h-[70vh] naxatw-w-full naxatw-py-2 md:naxatw-h-full md:naxatw-w-1/2">
+          <div className="naxatw-h-[70vh] naxatw-w-full naxatw-py-2 md:naxatw-h-full md:naxatw-w-2/3">
             <ProjectsMapSection />
           </div>
         )}
