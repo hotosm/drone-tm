af32dda4feb77faaf1030b959425b8fef642e1cf
Sujit <sujitkarki703@gmail.com>
2024-10-22 16:46:38 +0545

    feat(task-description): update UI of TaskDescription

--- src/frontend/src/views/TaskDescription/index.tsx
+++ src/frontend/src/views/TaskDescription/index.tsx
@@ -8,9 +8,9 @@ const TaskDescription = () => {
   const { width } = useWindowDimensions();
   return (
     <>
-      <div className="naxatw-h-screen-nav">
+      <div className="main-content">
         <div className="naxatw-mx-auto naxatw-px-3 naxatw-pb-[2.9375rem] lg:naxatw-px-20">
-          <div className="naxatw-mt-8 naxatw-flex naxatw-flex-col naxatw-gap-3">
+          <div className="naxatw-flex naxatw-flex-col naxatw-gap-3 naxatw-pt-8">
             <DroneOperatorTaskHeader />
             <div className="naxatw-grid naxatw-grid-cols-1 naxatw-gap-5 sm:naxatw-grid-cols-[30%_70%]">
               <DroneOperatorDescriptionBox />
