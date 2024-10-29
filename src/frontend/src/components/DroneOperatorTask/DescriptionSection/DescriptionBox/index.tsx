b6be4e77dd1f41a64b492056016020e8351fa593
Sujit <sujitkarki703@gmail.com>
2024-10-29 09:39:45 +0545

    feat(task-description): add options to choose want to replace the image or add on existing imags while uploading images

--- src/frontend/src/components/DroneOperatorTask/DescriptionSection/DescriptionBox/index.tsx
+++ src/frontend/src/components/DroneOperatorTask/DescriptionSection/DescriptionBox/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
 import {
   useGetIndividualTaskQuery,
@@ -10,14 +11,22 @@ import { useMutation } from '@tanstack/react-query';
 import { postProcessImagery } from '@Services/tasks';
 import { formatString } from '@Utils/index';
 import { Button } from '@Components/RadixComponents/Button';
+import { Label } from '@Components/common/FormUI';
+import SwitchTab from '@Components/common/SwitchTab';
+import { setUploadedImagesType } from '@Store/actions/droneOperatorTask';
+import { useTypedSelector } from '@Store/hooks';
 import DescriptionBoxComponent from './DescriptionComponent';
 import QuestionBox from '../QuestionBox';
 import UploadsInformation from '../UploadsInformation';
 import UploadsBox from '../UploadsBox';
 
 const DescriptionBox = () => {
+  const dispatch = useDispatch();
   const [flyable, setFlyable] = useState('yes');
   const { taskId, projectId } = useParams();
+  const uploadedImageType = useTypedSelector(
+    state => state.droneOperatorTask.uploadedImagesType,
+  );
 
   const { data: taskWayPoints }: any = useGetTaskWaypointQuery(
     projectId as string,
@@ -215,8 +224,39 @@ const DescriptionBox = () => {
             </div>
           )}
           {taskAssetsInformation?.state === 'IMAGE_PROCESSING_FAILED' && (
-            <div className="">
-              <UploadsBox label="Re-upload Raw Image" />
+            <div className="naxatw-flex naxatw-flex-col naxatw-gap-1 naxatw-pb-4">
+              <Label>
+                <p className="naxatw-text-[0.875rem] naxatw-font-semibold naxatw-leading-normal naxatw-tracking-[0.0175rem] naxatw-text-[#D73F3F]">
+                  Upload Images
+                </p>
+              </Label>
+              <SwitchTab
+                options={[
+                  {
+                    name: 'image-upload-for',
+                    value: 'add',
+                    label: 'Add to existing',
+                  },
+                  {
+                    name: 'image-upload-for',
+                    value: 'replace',
+                    label: 'Replace existing',
+                  },
+                ]}
+                valueKey="value"
+                selectedValue={uploadedImageType}
+                activeClassName="naxatw-bg-red naxatw-text-white"
+                onChange={(selected: Record<string, any>) => {
+                  dispatch(setUploadedImagesType(selected.value));
+                }}
+              />
+              <p className="naxatw-px-1 naxatw-py-1 naxatw-text-xs">
+                Note:{' '}
+                {uploadedImageType === 'add'
+                  ? 'Uploaded images will be added with the existing images.'
+                  : 'Uploaded images will be replaced with all the existing images and starts processing.'}
+              </p>
+              <UploadsBox label="" />
             </div>
           )}
         </div>
