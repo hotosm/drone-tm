9e157f3b3d84e92763b98cc012c8b287181d3182
Sujit <sujitkarki703@gmail.com>
2024-11-14 13:50:16 +0545

    feat(project-creation-form): form wrapper UI update

--- src/frontend/src/components/CreateProject/index.tsx
+++ src/frontend/src/components/CreateProject/index.tsx
@@ -6,7 +6,7 @@ import CreateProjectHeader from './CreateProjectHeader';
 
 const CreateProject = () => {
   return (
-    <section className="naxatw-flex naxatw-h-screen-nav naxatw-flex-col naxatw-bg-grey-50 naxatw-px-32 naxatw-pt-5 xl:naxatw-px-48">
+    <section className="naxatw-flex naxatw-h-screen-nav naxatw-flex-col naxatw-bg-grey-50 naxatw-px-8 naxatw-pt-5 xl:naxatw-px-16">
       <CreateProjectHeader />
       <StepSwitcher data={stepSwticherData} />
       <CreateprojectLayout />
