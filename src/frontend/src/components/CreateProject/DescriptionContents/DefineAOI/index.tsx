26980c92ee99da2a95480913443d954292b7ef30
Sujit <sujitkarki703@gmail.com>
2024-11-14 14:56:55 +0545

    feat(project-creation-form): update define AOI form information and form content

--- src/frontend/src/components/CreateProject/DescriptionContents/DefineAOI/index.tsx
+++ src/frontend/src/components/CreateProject/DescriptionContents/DefineAOI/index.tsx
@@ -2,7 +2,7 @@ import { DefineAOIInfo } from '@Constants/createProject';
 
 export default function DefineAOI() {
   return DefineAOIInfo?.map(info => (
-    <div className="naxatw-px-2 naxatw-py-2" key={info.key}>
+    <div className="" key={info.key}>
       <p className="naxatw-text-body-btn">{info.key}</p>
       <p className="naxatw-py-1 naxatw-text-body-md">{info.description}</p>
     </div>
