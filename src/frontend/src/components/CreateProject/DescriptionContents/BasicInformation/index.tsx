095bcec6fa62f8da211433a68d7bbe77dd82c8de
Sujit <sujitkarki703@gmail.com>
2024-11-14 15:42:24 +0545

    feat(project-creation-form): add animaiton on info text

--- src/frontend/src/components/CreateProject/DescriptionContents/BasicInformation/index.tsx
+++ src/frontend/src/components/CreateProject/DescriptionContents/BasicInformation/index.tsx
@@ -1,10 +1,14 @@
+import { FlexColumn } from '@Components/common/Layouts';
+
 export default function BasicInformation() {
   return (
-    <div className="">
-      <p className="naxatw-text-body-btn">Basic Information</p>
-      <p className="naxatw-mt-2 naxatw-text-body-md">
-        Fill in your basic project information such as name and description
-      </p>
-    </div>
+    <FlexColumn gap={2} className="naxatw-animate-fade-up">
+      <div className="">
+        <p className="naxatw-text-body-btn">Basic Information</p>
+        <p className="naxatw-text-body-md">
+          Fill in your basic project information such as name and description
+        </p>
+      </div>
+    </FlexColumn>
   );
 }
--- src/frontend/src/components/CreateProject/DescriptionContents/GenerateTask/index.tsx
+++ src/frontend/src/components/CreateProject/DescriptionContents/GenerateTask/index.tsx
@@ -1,18 +1,18 @@
+import { FlexColumn } from '@Components/common/Layouts';
 import { taskGenerationGuidelines } from '@Constants/createProject';
 
 export default function GenerateTask() {
   return (
-    <div className="">
-      <p className="naxatw-text-body-btn">Generate task</p>
-      <p className="naxatw-mt-2 naxatw-text-body-md">
-        Split the task into smaller chunks based on the given dimensions to
-        ensure more efficient and precise data collection and analysis.
-      </p>
-      <br />
+    <FlexColumn gap={2} className="naxatw-animate-fade-up">
       <div>
-        <p className="naxatw-mt-2 naxatw-text-body-md">
-          {taskGenerationGuidelines?.title}
+        <p className="naxatw-text-body-btn">Generate task</p>
+        <p className="naxatw-text-body-md">
+          Split the task into smaller chunks based on the given dimensions to
+          ensure more efficient and precise data collection and analysis.
         </p>
+      </div>
+      <div>
+        <p className="naxatw-text-body-md">{taskGenerationGuidelines?.title}</p>
         <ol className="naxatw-flex naxatw-list-decimal naxatw-flex-col naxatw-gap-1 naxatw-px-2 naxatw-py-2">
           {taskGenerationGuidelines?.guidelines?.map(item => (
             <li key={item} className="naxatw-text-left naxatw-text-body-md">
@@ -21,6 +21,6 @@ export default function GenerateTask() {
           ))}
         </ol>
       </div>
-    </div>
+    </FlexColumn>
   );
 }
