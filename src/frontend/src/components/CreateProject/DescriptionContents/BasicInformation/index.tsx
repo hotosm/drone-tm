99bc0c91f7099c366302002f1d55b63f2aae9879
Sujit <sujitkarki703@gmail.com>
2024-11-14 14:10:04 +0545

    feat(project-creation-form): update basic information form information and form content

--- src/frontend/src/components/CreateProject/DescriptionContents/BasicInformation/index.tsx
+++ src/frontend/src/components/CreateProject/DescriptionContents/BasicInformation/index.tsx
@@ -1,6 +1,6 @@
 export default function BasicInformation() {
   return (
-    <div className="naxatw-px-10 naxatw-py-5">
+    <div className="">
       <p className="naxatw-text-body-btn">Basic Information</p>
       <p className="naxatw-mt-2 naxatw-text-body-md">
         Fill in your basic project information such as name and description
