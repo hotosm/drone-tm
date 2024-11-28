953f29d48fb21ef0a7d9703e1fa174ed90fe0cb8
Sujit <sujitkarki703@gmail.com>
2024-11-28 11:46:35 +0545

    feat: clear selectedDocumentDetails on document preview component unmount

--- src/frontend/src/components/Dashboard/RequestLogs/ModalContent/DocumentPreviewModal.tsx
+++ src/frontend/src/components/Dashboard/RequestLogs/ModalContent/DocumentPreviewModal.tsx
@@ -1,10 +1,20 @@
 import DocViewer, { DocViewerRenderers } from '@cyntler/react-doc-viewer';
+import { setCommonState } from '@Store/actions/common';
 import { useTypedSelector } from '@Store/hooks';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 const DocumentPreviewModal = () => {
+  const dispatch = useDispatch();
   const documentDetails = useTypedSelector(
     state => state.common.selectedDocumentDetails,
   );
+
+  useEffect(() => {
+    return () => {
+      dispatch(setCommonState({ selectedDocumentDetails: null }));
+    };
+  }, [dispatch]);
 
   return (
     <div className="naxatw-h-full naxatw-w-full">
