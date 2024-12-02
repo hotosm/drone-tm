659ea54848960555e1104f58fb1d50cbf773406d
Bijay Rauniyar <rauniyarbijay0@gmail.com>
2024-12-02 15:41:29 +0545

    feat: implementation of project avg taskway points count api

--- src/frontend/src/components/CreateProject/FormContents/GenerateTask/index.tsx
+++ src/frontend/src/components/CreateProject/FormContents/GenerateTask/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import { useMutation } from '@tanstack/react-query';
 import { useState } from 'react';
 import ErrorMessage from '@Components/common/ErrorMessage';
@@ -8,7 +9,10 @@ import { toast } from 'react-toastify';
 import { setCreateProjectState } from '@Store/actions/createproject';
 import { convertGeojsonToFile } from '@Utils/convertLayerUtils';
 import prepareFormData from '@Utils/prepareFormData';
-import { postPreviewSplitBySquare } from '@Services/createproject';
+import {
+  getProjectWayPoints,
+  postPreviewSplitBySquare,
+} from '@Services/createproject';
 import MapSection from './MapSection';
 
 export default function GenerateTask({ formProps }: { formProps: any }) {
@@ -16,6 +20,14 @@ export default function GenerateTask({ formProps }: { formProps: any }) {
   const [error, setError] = useState('');
 
   const { register, watch } = formProps;
+  const {
+    front_overlap,
+    side_overlap,
+    altitude_from_ground,
+    gsd_cm_px,
+    outline,
+  } = watch();
+
   const dimension = watch('task_split_dimension');
 
   const projectArea = useTypedSelector(
@@ -33,6 +45,16 @@ export default function GenerateTask({ formProps }: { formProps: any }) {
     dimension,
     ...(noFlyZone ? { no_fly_zones: noFlyZoneGeojsonFile } : {}), // add no fly zones it there are any
   });
+
+  const { mutate: mutateProjectWayPoints, data: projectWayPoints } =
+    useMutation({
+      mutationFn: (projectGeoJsonPayload: Record<string, any>) => {
+        const { project_geojson, ...params } = projectGeoJsonPayload;
+        return getProjectWayPoints(params, {
+          project_geojson,
+        });
+      },
+    });
 
   const { mutate, isLoading } = useMutation<any, any, any, unknown>({
     mutationFn: postPreviewSplitBySquare,
@@ -82,11 +104,25 @@ export default function GenerateTask({ formProps }: { formProps: any }) {
                 capturedProjectMap: false,
               }),
             );
+            const projectWayPointsPayload = {
+              front_overlap: front_overlap || 0,
+              side_overlap: side_overlap || 0,
+              altitude_from_ground: altitude_from_ground || 0,
+              gsd_cm_px: gsd_cm_px || 0,
+              project_geojson: convertGeojsonToFile(outline),
+            };
+            mutateProjectWayPoints(projectWayPointsPayload);
             return mutate(payload);
           }}
         >
           Generate Task
         </Button>
+        <p className="naxatw-mt-4 naxatw-text-sm naxatw-font-semibold">
+          The average number of way points is{' '}
+          <span className="naxatw-text-red">
+            {projectWayPoints?.data?.avg_no_of_waypoints}
+          </span>
+        </p>
       </div>
       <div className="naxatw-col-span-3 naxatw-h-[50vh] naxatw-bg-green-50 md:naxatw-col-span-2 md:naxatw-h-full">
         <MapSection />
