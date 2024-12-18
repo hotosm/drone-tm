d46c6a578d797d5114be74bad6f3c7e8f7a1ec32
Pradip-p <thapapradip542@gmail.com>
2024-12-18 15:12:16 +0545

    feat: updated test case for read drone

--- src/backend/tests/test_drones_routes.py
+++ src/backend/tests/test_drones_routes.py
@@ -12,6 +12,19 @@ async def test_create_drone(client, drone_info):
     return response.json()
 
 
+@pytest.mark.asyncio
+async def test_read_drone(client, drone_info):
+    """Test retrieving a drone record."""
+
+    response = await client.post("/api/drones/create-drone", json=drone_info)
+    assert response.status_code == HTTPStatus.OK
+    drone_id = response.json().get("drone_id")
+    response = await client.get(f"/api/drones/{drone_id}")
+    assert response.status_code == HTTPStatus.OK
+    drone_data = response.json()
+    assert drone_data.get("model") == drone_info["model"]
+
+
 if __name__ == "__main__":
     """Main func if file invoked directly."""
     pytest.main()
