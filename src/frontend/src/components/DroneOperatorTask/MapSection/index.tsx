0c163a26a85118eba43e09d316f612f073b4fca2
Sujit <suzeett10@gmail.com>
2024-12-30 13:22:50 +0545

    fix: download menu hidden on mobile view
     reduce waypoint mode switcher z-index

--- src/frontend/src/components/DroneOperatorTask/MapSection/index.tsx
+++ src/frontend/src/components/DroneOperatorTask/MapSection/index.tsx
@@ -839,7 +839,7 @@ const MapSection = ({ className }: { className?: string }) => {
             getCoordOnProperties
           />
 
-          <div className="naxatw-absolute naxatw-right-3 naxatw-top-3 naxatw-z-40 lg:naxatw-right-64">
+          <div className="naxatw-absolute naxatw-right-3 naxatw-top-3 naxatw-z-10 lg:naxatw-right-64">
             <SwitchTab
               activeClassName="naxatw-bg-red naxatw-text-white"
               options={waypointModeOptions}
