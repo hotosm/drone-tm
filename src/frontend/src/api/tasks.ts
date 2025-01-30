0e0403043819f98563d0f183afbe7aeea5b09d89
Sujit <suzeett10@gmail.com>
2025-01-30 15:43:10 +0545

    feat(task-description): use separate api for assets data insted of using task description data

--- src/frontend/src/api/tasks.ts
+++ src/frontend/src/api/tasks.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 import {
   getIndividualTask,
-  // getTaskAssetsInfo,
+  getTaskAssetsInfo,
   getTaskWaypoint,
 } from '@Services/tasks';
 import { useQuery, UseQueryOptions } from '@tanstack/react-query';
@@ -35,16 +35,16 @@ export const useGetIndividualTaskQuery = (
   });
 };
 
-// export const useGetTaskAssetsInfo = (
-//   projectId: string,
-//   taskId: string,
-//   queryOptions?: Partial<UseQueryOptions>,
-// ) => {
-//   return useQuery({
-//     queryKey: ['task-assets-info'],
-//     enabled: !!taskId,
-//     queryFn: () => getTaskAssetsInfo(projectId, taskId),
-//     select: (res: any) => res.data,
-//     ...queryOptions,
-//   });
-// };
+export const useGetTaskAssetsInfo = (
+  projectId: string,
+  taskId: string,
+  queryOptions?: Partial<UseQueryOptions>,
+) => {
+  return useQuery({
+    queryKey: ['task-assets-info'],
+    enabled: !!taskId,
+    queryFn: () => getTaskAssetsInfo(projectId, taskId),
+    select: (res: any) => res.data,
+    ...queryOptions,
+  });
+};
