4ea36964bb19c66bf05d26d58fe02ab63629895b
spwoodcock <sam.woodcock@protonmail.com>
2025-02-10 14:54:17 +0000

    fix(frontend): import hotosm/ui javascript files, add custom type declaration

---
+++ src/frontend/src/types/custom.d.ts
@@ -0,0 +1,8 @@
+declare module '@hotosm/ui/dist/hotosm-ui';
+declare module '*.png';
+declare module '*.svg';
+declare module '*.jpeg';
+declare module '*.jpg';
+declare module 'uuid';
+declare module '@mapbox/mapbox-gl-draw-static-mode';
+declare module 'mapbox-gl-draw-cut-line-mode';
