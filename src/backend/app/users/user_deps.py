c679a4aea53ede3080fb39f5d647e4faf63e3509
Niraj Adhikari <nrjadkry@gmail.com>
2025-02-14 10:51:20 +0545

    updated login dependencies

--- src/backend/app/users/user_deps.py
+++ src/backend/app/users/user_deps.py
@@ -4,9 +4,13 @@ from fastapi import HTTPException, Request, Security
 from fastapi.security.api_key import APIKeyHeader
 from app.config import settings
 from app.users.auth import Auth
-from app.users.user_schemas import AuthUser
+from app.users.user_schemas import AuthUser, DbUser
 from loguru import logger as log
 from datetime import datetime, timedelta
+from typing import Annotated
+from psycopg import Connection
+from fastapi import Depends
+from app.db import database
 
 
 async def init_google_auth():
@@ -27,30 +31,37 @@ async def init_google_auth():
     )
 
 
-async def login_required(
-    request: Request, access_token: str = Security(APIKeyHeader(name="access-token"))
-) -> AuthUser:
-    """Dependency to inject into endpoints requiring login."""
-    # if settings.DEBUG:
-    #     return AuthUser(
-    #         id="6da91a51-5efd-40c9-a9c4-b66465a75fbe",
-    #         email="admin@hotosm.org",
-    #         name="admin",
-    #         profile_img="",
-    #         role="",
-    #     )
-
+async def verify_access_token(
+    access_token: str = Security(APIKeyHeader(name="access-token")),
+) -> dict:
+    """Common token verification logic."""
     if not access_token:
         raise HTTPException(status_code=401, detail="No access token provided")
 
     try:
         user = verify_token(access_token)
+        return user
     except HTTPException as e:
         log.error(e)
         log.error("Failed to verify access token")
         raise HTTPException(status_code=401, detail="Access token not valid") from e
 
-    return AuthUser(**user)
+
+async def login_required(
+    request: Request,
+    user_dict: dict = Depends(verify_access_token),
+) -> AuthUser:
+    """Dependency to inject into endpoints requiring login."""
+    return AuthUser(**user_dict)
+
+
+async def login_dependency(
+    db: Annotated[Connection, Depends(database.get_db)],
+    request: Request,
+    user_dict: dict = Depends(verify_access_token),
+) -> DbUser:
+    """Dependency to inject into endpoints requiring login with database user."""
+    return await DbUser.get_or_create_user(db, AuthUser(**user_dict))
 
 
 def create_reset_password_token(email: str):
