79382f984375732172a4266b11ac30f7449905ea
spwoodcock <sam.woodcock@protonmail.com>
2025-04-07 22:35:00 +0100

    fix(drone-flightplan): add __all__ for top level imports from pkg

--- src/backend/packages/drone-flightplan/drone_flightplan/__init__.py
+++ src/backend/packages/drone-flightplan/drone_flightplan/__init__.py
@@ -2,13 +2,6 @@
 
 import os
 
-from drone_flightplan.add_elevation_from_dem import add_elevation_from_dem
-from drone_flightplan.calculate_parameters import calculate_parameters
-from drone_flightplan.create_flightplan import create_flightplan
-from drone_flightplan.create_placemarks import create_placemarks
-from drone_flightplan.waypoints import create_waypoint
-from drone_flightplan.wpml import create_wpml
-
 __all__ = [
     "add_elevation_from_dem",
     "calculate_parameters",
