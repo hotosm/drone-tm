e54927344a1c2c8229b772a13bd87bbff37cdfc4
Stanley Liu <sliu1993@hotmail.co.uk>
2025-12-01 23:47:48 -0500

    test/refactor: remove unused drone endpoints and tests

--- src/backend/app/drones/drone_routes.py
+++ src/backend/app/drones/drone_routes.py
@@ -1,15 +1,10 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from psycopg import Connection
 
 from app.db import database
-from app.drones import drone_deps, drone_schemas
-from app.models.enums import HTTPStatus
-from app.users.permissions import (
-    IsSuperUser,
-    check_permissions,
-)
+from app.drones import drone_schemas
 from app.users.user_deps import login_required
 from app.users.user_schemas import AuthUser
 
@@ -18,80 +13,6 @@ router = APIRouter(
     tags=["Drones"],
     responses={404: {"description": "Not found"}},
 )
-
-
-@router.get("/", response_model=list[drone_schemas.DroneOut])
-async def read_drones(
-    db: Annotated[Connection, Depends(database.get_db)],
-):
-    """Get all drones."""
-    try:
-        return await drone_schemas.DbDrone.all(db)
-    except KeyError as e:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND) from e
-
-
-@router.post("/create-drone")
-async def create_drone(
-    drone_info: drone_schemas.DroneIn,
-    db: Annotated[Connection, Depends(database.get_db)],
-    user_data: Annotated[AuthUser, Depends(check_permissions(IsSuperUser()))],
-):
-    """Create a new drone in database"""
-    drone_id = await drone_schemas.DbDrone.create(db, drone_info)
-    return {"message": "Drone created successfully", "drone_id": drone_id}
-
-
-@router.delete("/{drone_id}")
-async def delete_drone(
-    drone: Annotated[drone_schemas.DbDrone, Depends(drone_deps.get_drone_by_id)],
-    db: Annotated[Connection, Depends(database.get_db)],
-    user_data: Annotated[AuthUser, Depends(check_permissions(IsSuperUser()))],
-):
-    """Deletes a drone record from the database.
-
-    Args:
-        drone_id (int): The ID of the drone to be deleted.
-        db (Database, optional): The database session object.
-        user_data (AuthUser, optional): The authenticated user data.
-
-    Returns:
-        dict: A success message if the drone was deleted.
-    """
-    drone_id = await drone_schemas.DbDrone.delete(db, drone.id)
-    return {"message": f"Drone successfully deleted {drone_id}"}
-
-
-@router.get("/{drone_id}", response_model=drone_schemas.DbDrone)
-async def read_drone(
-    drone: Annotated[drone_schemas.DbDrone, Depends(drone_deps.get_drone_by_id)],
-    db: Annotated[Connection, Depends(database.get_db)],
-    user_data: Annotated[AuthUser, Depends(login_required)],
-):
-    """Retrieves a drone record from the database.
-
-    Args:
-        drone_id (int): The ID of the drone to be retrieved.
-        db (Database, optional): The database session object.
-        user_data (AuthUser, optional): The authenticated user data.
-
-    Returns:
-        dict: The drone record if found.
-    """
-    return drone
-
-
-@router.get("/drone-altitude/")
-async def get_all_altitudes(
-    db: Annotated[Connection, Depends(database.get_db)],
-    user_data: Annotated[AuthUser, Depends(login_required)],
-):
-    """Retrieves all drone altitude regulations."""
-    altitudes = await drone_schemas.DroneFlightHeight.all(db)
-    if not altitudes:
-        return []
-    return altitudes
-
 
 @router.get("/drone-altitude/{country}/")
 async def get_drone_altitude_by_country(
--- src/backend/tests/test_drones_routes.py
+++ src/backend/tests/test_drones_routes.py
@@ -4,25 +4,13 @@ from app.models.enums import HTTPStatus
 
 
 @pytest.mark.asyncio
-async def test_create_drone(client, drone_info):
+async def test_get_drone_altitude_by_country(client, drone_info):
     """Create a new project."""
-    response = await client.post("/drones/create-drone", json=drone_info)
+    country = 'canada'
+    response = await client.post(f"/api/drone-altitude/${country}", json=drone_info)
     assert response.status_code == HTTPStatus.OK
 
     return response.json()
-
-
-@pytest.mark.asyncio
-async def test_read_drone(client, drone_info):
-    """Test retrieving a drone record."""
-    response = await client.post("/api/drones/create-drone", json=drone_info)
-    assert response.status_code == HTTPStatus.OK
-    drone_id = response.json().get("drone_id")
-    response = await client.get(f"/api/drones/{drone_id}")
-    assert response.status_code == HTTPStatus.OK
-    drone_data = response.json()
-    assert drone_data.get("model") == drone_info["model"]
-
 
 if __name__ == "__main__":
     """Main func if file invoked directly."""
