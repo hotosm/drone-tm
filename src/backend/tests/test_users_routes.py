2812aa8354279ffe295e6606136a5aa057c63da0
allison <114789954+allisonsibrian@users.noreply.github.com>
2025-12-16 18:42:46 -0800

    fix(backend): ensure existing tests all pass (#689)
    * fixing backend tests
    
    * fixing AttributeError, and is_superuser field
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/backend/tests/test_users_routes.py
+++ src/backend/tests/test_users_routes.py
@@ -6,6 +6,7 @@ import pytest_asyncio
 from loguru import logger as log
 
 from app.config import settings
+from app.users.user_deps import create_reset_password_token
 
 
 @pytest_asyncio.fixture(scope="function")
@@ -20,12 +21,13 @@ def token(auth_user):
 
 
 @pytest.mark.asyncio
-async def test_reset_password_success(client, token):
+async def test_reset_password_success(client, auth_user):
     """Test successful password reset using a valid token."""
+    token = create_reset_password_token(auth_user.email_address)
     new_password = "QPassword@12334"
 
     response = await client.post(
-        f"/api/users/reset-password?token={token}&new_password={new_password}"
+        f"/users/reset-password/?token={token}&new_password={new_password}"
     )
 
     if response.status_code != 200:
