6b188e362c21e46a61322abc7025d6a150bf9b10
Stanley Liu <sliu1993@hotmail.co.uk>
2025-12-22 19:24:52 -0500

    test: update  drone test file ensuring that the drone altitude is fetched correctly

--- src/backend/tests/test_drones_routes.py
+++ src/backend/tests/test_drones_routes.py
@@ -4,10 +4,10 @@ from app.models.enums import HTTPStatus
 
 
 @pytest.mark.asyncio
-async def test_get_drone_altitude_by_country(client, drone_info):
-    """Create a new project."""
+async def test_get_drone_altitude_by_country(client):
+    """Test getting drone altitude by country."""
     country = 'canada'
-    response = await client.post(f"/api/drone-altitude/${country}", json=drone_info)
+    response = await client.get(f"/api/drones/drone-altitude/{country}/")
     assert response.status_code == HTTPStatus.OK
 
     return response.json()
